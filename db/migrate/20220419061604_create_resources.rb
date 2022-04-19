4ddf72093d27cf6db2c0ed574695078207856359
michael <software.engr.full.stack@gmail.com>
2022-04-18 23:16:36 -0700

    Add resource model

---
+++ db/migrate/20220419061604_create_resources.rb
@@ -0,0 +1,11 @@
+class CreateResources < ActiveRecord::Migration[7.0]
+  def change
+    create_table :resources do |t|
+      t.string :name
+      t.text :data, :jsonb
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_19_061327) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_19_061604) do
+  create_table "resources", force: :cascade do |t|
+    t.string "name"
+    t.text "data"
+    t.text "jsonb"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_resources_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.text "data"
@@ -19,4 +29,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_19_061327) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "resources", "users"
 end
